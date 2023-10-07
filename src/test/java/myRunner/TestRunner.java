0e97d4406efcd6a44fd18817dbd91ec0fbbdb5aa
Abhinav19881209 <abhinav.khadatkar20@gmail.com>
2023-10-07 21:44:32 +0530

    extent report

--- src/test/java/myRunner/TestRunner.java
+++ src/test/java/myRunner/TestRunner.java
@@ -9,8 +9,9 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 				features="src/test/resource/featuress/Login.feature",
 				glue= {"stepDefinition","hooks"},
-				plugin= {"pretty","html:target/CucumberReports/Reports.html"},
-				tags = "@Login"
+				plugin= {"pretty","html:target/CucumberReports/Reports.html",
+						"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+				//tags = "@Login"
 				)
 public class TestRunner {
 
